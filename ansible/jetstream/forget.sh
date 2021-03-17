8b3fd57be1715fe93baf777fb0f683653762adb6
Keith Suderman <suderman@cs.vassar.edu>
2021-03-16 20:22:09 -0400

    Updating playbooks

--- ansible/jetstream/forget.sh
+++ ansible/jetstream/forget.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
-for ip in `cat inventories/galaxy.ini | grep _host | cut -d= -f2` ; do
+if [[ -z "$KEY" ]] ; then
+	KEY=~/.ssh/ks-cluster.pem
+fi
+
+INV=inventory.ini
+if [[ -n  "$1" ]] ; then
+	INV=$1
+fi
+
+function connect_to_server() {
+	for tries in 1 2 3 4 5 ; do
+		echo "$tries) Attempting an SSH connection to $1"
+		ssh -i $KEY -o "StrictHostKeyChecking no" ubuntu@$1 uname -a
+		if [[ $? = 0 ]] ; then	
+			return
+		fi
+		echo "Pausing"
+		sleep 10
+	done
+	echo "Unable to SSH to $1"
+	exit 1
+}
+
+for ip in $(cat inventories/$INV | grep _host | cut -d= -f2) ; do
 	ssh-keygen -R $ip
-	ssh -i ~/.ssh/release-testing-2101.pem ubuntu@$ip uname -a
+	#ssh -i ~/.ssh/ks-cluster.pem -o "StrictHostKeyChecking no" ubuntu@$ip uname -a
+	connect_to_server $ip
 done
